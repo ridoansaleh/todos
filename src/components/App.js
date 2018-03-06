f77b9b6b48aa2e3589c920851ef36c196b172886
ridoansaleh <ridoansalehnasution98@gmail.com>
2018-03-06 10:06:08 +0700

    add styling

--- src/components/App.js
+++ src/components/App.js
@@ -2,9 +2,10 @@ import React from 'react'
 import Footer from './Footer'
 import AddTodo from '../containers/AddTodo'
 import VisibleTodoList from '../containers/VisibleTodoList'
+import '../css/App.css'
 
 const App = () => (
-  <div>
+  <div className='container'>
     <AddTodo />
     <VisibleTodoList />
     <Footer />
