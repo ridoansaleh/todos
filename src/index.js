82b816b10eeac6a049fad35ae9e00a94b20e255b
ridoansaleh <ridoansalehnasution98@gmail.com>
2018-03-10 11:17:58 +0700

    add notes

--- src/index.js
+++ src/index.js
@@ -17,7 +17,7 @@ render(
 
 /* Notes 
 
-  1. Provider : the higher-order component provided by React Redux that lets you bind Redux to React.
+  1. Provider : the higher-order component (HoC) provided by React Redux that lets you bind Redux to React.
   2. store    : is an object that brings actions and reducers together.
      store has responsibilities : 
       > Holds application state;
@@ -25,8 +25,9 @@ render(
       > Allows state to be updated via dispatch(action);
       > Registers listeners via subscribe(listener);
       > Handles unregistering of listeners via the function returned by subscribe(listener)
-  3. reducer  : is a pure function that has 2 params which are state and actions. 
-                Most importantly, reducer return the next state. 
-
+  3. reducer     : is a pure function that has 2 params which are state and actions. 
+                   Most importantly, reducer return the next state. 
+  4. createStore : is a function that imported from redux that let you create a store from reducers. 
+                   store is a state tree of react-redux-app.
 
 */
